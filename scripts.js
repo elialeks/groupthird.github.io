783172e892b528fb5c2d163e37f6721d58a04cd1
elialeks <62053141+elialeks@users.noreply.github.com>
2024-12-17 00:20:22 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,34 @@
+document.querySelectorAll('.accordion-header').forEach(header => {
+  header.addEventListener('click', () => {
+    const content = header.nextElementSibling;
+
+    // Toggle visibility of the clicked section
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+      header.classList.remove('active');
+    } else {
+      content.style.display = 'block';
+      header.classList.add('active');
+    }
+  });
+});
+
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+// Show the button when the user scrolls down 100px
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollToTopBtn.style.display = 'block'; // Show the button
+  } else {
+    scrollToTopBtn.style.display = 'none'; // Hide the button
+  }
+};
+
+// Scroll to top function
+scrollToTopBtn.addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // This will make the scroll smooth
+  });
+});
